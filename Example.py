7364ee18a69b6d8485374db9f3a67bce4f981666
NikitaPal19 <122282829+NikitaPal19@users.noreply.github.com>
2025-05-13 11:38:56 +0530

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,1 +1,13 @@
 print("Try 1")
+
+print("⭐ Star Pattern")
+for i in range(1, 6):
+    print("*" * i)
+
+print("\n✅ Prime numbers from 1 to 20:")
+for num in range(2, 21):
+    for i in range(2, num):
+        if num % i == 0:
+            break
+    else:
+        print(num)
